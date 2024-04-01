1d93e7e7319334ab3b2305cb6424c5e92a0af2d0
Victor Hugo <victorhugo156@gmail.com>
2024-04-01 11:45:50 +1100

    I have finished the information section responsivess

--- resume/main.js
+++ resume/main.js
@@ -1,7 +1,43 @@
+
+
 const btnMenu = document.querySelector(".menu-icon");
 let btnClose = document.querySelector(".btn-close");
 const wrapper = document.querySelector(".mobile-list");
 const description = document.querySelector(".container-content");
+
+/*
+const observer = new IntersectionObserver((entries)=>{
+  console.log(entries)
+ 
+  Array.from(entries).forEach( entry =>{
+      entries.intersectionRatio
+      entry.target.classList.add('init-hidden-off')
+ 
+  }, {
+    threshold: [0, .5, 1]
+  })
+  
+})
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element =>{
+  observer.observe(element)
+  
+})*/
+
+const header = document.querySelector('.header')
+const navHeight = header.offsetHeight
+console.log(navHeight)
+
+function changeHeaderWhenScroll(){
+  if(window.scrollY >= navHeight){
+    header.classList.add('scroll')
+    console.log("I'm into the if")
+  }else{
+    header.classList.remove('scroll')
+    console.log("Im into the else")
+  }
+}
+
 
 btnMenu.addEventListener("click", function(){
   wrapper.classList.toggle("active");
