89b40257ba56bd2b8fcf548cada3d357aaec1fac
Victor Hugo <victorhugo156@gmail.com>
2024-06-14 07:52:12 +1000

    Footer Animation

--- resume/main.js
+++ resume/main.js
@@ -2,13 +2,31 @@
 
 const btnMenu = document.querySelector(".menu-icon");
 let btnClose = document.querySelector(".btn-close");
+const btnAboutMe = document.querySelector("#modal-about-me")
+const btnAboutMeClose = document.querySelector(".aboutMe-close");
 const wrapper = document.querySelector(".mobile-list");
-const wraperMain = document.querySelector(".test");
+const wraperMain = document.querySelector(".scroll-review");
 const fader = document.querySelector(".wrap-footer-content");
-const element = document.querySelector(".hidden")
+const element = document.querySelector(".hidden");
+const modal = document.querySelector("dialog");
+
+/*------------Function Modal About Me------------*/
+
+btnAboutMe.onclick = function(){
+  modal.showModal();
+}
+
+btnAboutMeClose.onclick = function(){
+  modal.close();
+  console.log("Im here")
+}
+
+
+
+/*------------Function Scroll Review Footer------------*/
 
 const appearOptions = {
-  threshold: 0.2,
+  threshold: .3,
   // rootMargin: "-955px"
 
   // rootMargin: "-395px"
@@ -17,7 +35,7 @@ const appearOptions = {
 const apperOnScroll = new IntersectionObserver((entries)=>{
 
   entries.forEach((entry)=>{
-    if(!entry.isIntersecting){
+    if(entry.isIntersecting){
       // entry.target.classList.remove('show');
       fader.classList.add('show');
       console.log("This is the if: " + entry)
@@ -36,10 +54,8 @@ const apperOnScroll = new IntersectionObserver((entries)=>{
 
 
 
+/*------------Function Open & Close Menu Mobile------------*/
 
-
-
-//Open & Close Menu Mobile
 btnMenu.addEventListener("click", function(){
   wrapper.classList.toggle("active");
   //wrapper.classList.add("active");
